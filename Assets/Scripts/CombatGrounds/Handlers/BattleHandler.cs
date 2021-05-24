5ff4ac0d2cfd6f302096a9b3ab215ad7cb7b9373
Seth B <sethbradley97@gmail.com>
2021-05-23 20:57:44 -0700

    FSM progression Origin

--- Assets/Scripts/CombatGrounds/Handlers/BattleHandler.cs
+++ Assets/Scripts/CombatGrounds/Handlers/BattleHandler.cs
@@ -14,6 +14,7 @@ public class BattleHandler : MonoBehaviour
     public List<PlayerUnit> playerActionQueue;
     public InputHandler inputHandler;
     //
+    //
 #endregion    
     private void Start() 
     {
